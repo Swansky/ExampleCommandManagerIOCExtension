ff9311380792e973f0861958cb7e53626cbbd1f3
ZOLTOWSKI Valérian <valerian.zoltowski@gmail.com>
2021-10-16 22:39:53 +0200

    stupid man

--- src/main/java/constants/Constants.java
+++ src/main/java/constants/Constants.java
@@ -2,5 +2,5 @@ package constants;
 
 public class Constants {
     public final static String TAG = "S!";
-    public final static String TOKEN = "MzUyMDIxMjA1NTEzMTQyMjc0.WaUzSw.jBFdxcFjrhX8iLUO6tRn91Ua-ig";
+    public final static String TOKEN = "";
 }
